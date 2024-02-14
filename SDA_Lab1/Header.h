0a2659483a506c6ea10e158f819cb27f7f4f4562
rima-i31 <143452410+rima-i31@users.noreply.github.com>
2024-02-14 14:17:05 +0200

    14.02.24
    Без вставки в рандомное место в массиве, при этом есть функция чтения/вставки в файл в коментах.

--- SDA_Lab1/Header.h
+++ SDA_Lab1/Header.h
@@ -4,7 +4,6 @@
 //
 //  Created by Rima Mihova on 03.02.2024.
 //
-
 #ifndef Header_h
 #define Header_h
 
@@ -12,20 +11,20 @@ struct Film {
     char* name;
     int year;
 };
+typedef Film* PFilm;
+
+void inputFilm(Film* f, int i, Film *a[]);
+void printFilm(Film *a[], int n);
+void freeFilm(Film* f,int &n);
+void insertFilm(Film* &films, int &n, Film *a[]);
+void filmSort(Film *a[], int n);
+void filmYearSort(Film *a[], int n);
+int compareFilms(char *a, char *b);
+void deleteFilm(Film* &films, int &n, int index);
+void redactFilm(Film* films, int n, int index);
+void searchFilmByName(Film *a[], int n, char* name);
+void searchFilmByYear(Film *a[], int n, int year);
+void writeFilmsToFile(const char* filename, Film *a[], int n);
+//void readFilmsFromFile(const char* filename, Film** a, int& n);
 
 #endif /* Header_h */
-
-
-
-
-
-void inputFilm(Film* f) ;
-void printFilm(const Film& f,int i);
-void freeFilm(Film* f);
-void deleteFilm(Film* &films, int &n, int index) ;
-void insertFilm(Film* &films, int &n) ;
-void addFilmAfter(Film* &films, int &n, int index);
-void filmSort(Film a[],int n);
-int compareFilms(char *a, char *b);
-void redactFilm(Film* &films, int &n,int i);
-void writeToFile(Film* &films, int n);
--- SDA_Lab1/function.cpp
+++ SDA_Lab1/function.cpp
@@ -5,6 +5,7 @@
 //  Created by Rima Mihova on 03.02.2024.
 //
 
+#include<fstream>
 #include <stdio.h>
 #include <iostream>
 #include "Header.h"
@@ -12,23 +13,49 @@
 using namespace std;
 
 //Функция позволяющая ввести с клавиатуры элементв структуры с помощью динамического выделения памяти
-void inputFilm(Film* f) {
+void inputFilm(Film* f,int i, Film* a[]) {
     cout << "Введите название фильма: ";
     string name;
     getline(cin, name);
-    f->name = new char[name.length() + 1];
-    strcpy(f->name, name.c_str());
+    f[i].name = new char[name.length() + 1];
+    strcpy(f[i].name, name.c_str());
     cout << "Введите год выпуска: ";
-    cin >> f->year;
+    while (true) {
+        cin >> f[i].year;
+        if (cin.fail()) {
+            cout << "Ошибка: Год должен быть числом. Пожалуйста, введите год снова: ";
+            cin.clear(); // Сбрасываем флаг ошибки ввода
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Очищаем буфер ввода до новой строки
+        } else {
+            break; // Если считывание прошло успешно, выходим из цикла
+        }
+    }
+    a[i] = &f[i];
     cin.ignore(); // Очистка буфера после ввода числа
 }
-//Функция, выводящая элементы структуры(имя  и в скобках год выпуска)
-void printFilm(const Film& f,int i) {
-    cout << i+1<<"."<<f.name << " (" << f.year << ")\n";
+
+void insertFilm(Film* &films, int &n, PFilm *a) {
+    inputFilm(films,n,a);
+    n++;
 }
+
+
+//Функция, выводящая элементы структуры(имя  и в скобках год выпуска)
+void printFilm(Film *a[], int n) {
+    if(n>0){
+        cout<<"Cписок фильмов:\n";
+        for(int i = 0;i<n;i++){
+            cout << i+1<<"."<<a[i]->name << " (" << a[i]->year << ")\n";
+        }
+    }else cout<<"Список пуст.\n";
+    
+}
+
 // Функция для освобождения памяти, выделенной для структуры film
-void freeFilm(Film* f) {
-    delete[] f->name;
+void freeFilm(Film* f,int &n) {
+    for (int i = 0; i < n; i++) {
+        delete[] f->name;
+    }
 }
 
 // Функция для сравнения названий фильмов при сортировке
@@ -37,100 +64,138 @@ int compareFilms(char *a, char *b) {
         *a++;*b++;
     }
     if(*a<*b)
-        return 1;
-    else if(*a>*b)
         return -1;
+    else if(*a>*b)
+        return 1;
     else return 0;
     
 }
 
-
-void filmSort(Film a[],int n){
-    int i ,j,nMin;
-    char *c;
-    
-    for(i=0;i<n;i++){
-        nMin=i;
-        for(j=i;j<n;j++){
-            //if(strcmp(a[j].name, a[nMin].name))
-            if(compareFilms(a[j].name, a[nMin].name))
-                nMin = j;
-        }
-        c=a[i].name;a[i].name=a[nMin].name; a[nMin].name=c;
-    }
-    
-}
-// Функция для удаления указанного элемента из массива
-void deleteFilm(Film* &films, int &n, int index) {
-    //Проверка на наличие данного номера
-    if (index < 0 || index > n) {
-        cout << "Недествительный номер\n";
-        return;
-    }
-    //Копирование существующих фильмов в новый массив, меньший исходного на 1 элемент, без элемента под введенным индексом
-    Film* newFilms = new Film[n - 1];
-    for (int i = 0, j = 0; i < n; i++) {
-        if (i != index-1) {
-            newFilms[j] = films[i];
-            j++;
+void filmSort(Film *a[], int n) {
+    int i, j;
+    if(n>0){
+        for (i = 0; i < n-1; i++) {
+            for (j = 0; j < n-i-1; j++) {
+                if (compareFilms(a[j]->name, a[j+1]->name) > 0) {
+                    // Обмен указателями
+                    Film *temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
         }
     }
-    delete[] films; // освобождение памяти исходного массива
-    films = newFilms;// присваивание нового массива
-    n--;//уменьшение объема массива на 1 элемент
+   
 }
-//Функция добавления элемента после необходимого
-void addFilmAfter(Film* &films, int &n, int index) {
-    //Проверка на наличие данного номера
-    if (index-1 < 0 || index-1 >n) {
-        cout << "Недествительный номер\n";
-        return;
-    }
-    //Копирование существующих фильмов в новый массив, ,больший исходного на 1 элемент, с добавлением под введенным индексом
-    Film* newFilms = new Film[n + 1];
-    // Копирование существующих фильмов в новый массив
-    int i = 0;int j = 0;
-    while(i < n) {
-        if(index-1 == i){
-            inputFilm(&newFilms[i]);j++;
+void filmYearSort(Film *a[], int n) {
+    int i, j;
+    if(n>0){
+        for (i = 0; i < n-1; i++) {
+            for (j = 0; j < n-i-1; j++) {
+                if (a[j]->year > a[j+1]->year) {
+                    // Обмен указателями
+                    Film *temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
         }
-        newFilms[j++] = films[i++];
     }
-    
-    delete[] films; // освобождение памяти исходного массива
-    films = newFilms;// присваивание нового массива
-    n++;//уменьшение объема массива на 1 элемент
+   
 }
-//Функция для вставки фильма
-void insertFilm(Film* &films, int &n) {
-    Film* newFilms = new Film[n + 1];
-    
-    // Копирование существующих фильмов в новый массив
+void searchFilmByName(Film *a[], int n, char* name) {
+    bool found = false;
     for (int i = 0; i < n; i++) {
-        newFilms[i] = films[i];
+        if (compareFilms(a[i]->name, name) == 0) {
+            cout << "Фильмы \"" << name << "\" найдены:\n";
+            cout << i + 1 << a[i]->year<<"\n";
+            found = true;
+        }
     }
-
-    // Добавление нового элемента
-    inputFilm(&newFilms[n]);
-
-    delete[] films; // освобождение памяти исходного массива
-    films = newFilms; // присваивание нового массива
-    n++;//увеличение объема массива на 1 элемент
-}
-
-//Функция для редактации фильма
-void redactFilm(Film* &films, int &n,int i){
-    addFilmAfter(films, n,i);
-    deleteFilm(films, n, i+1);
-}
-// Функция для записи массива структур в файл
-void writeToFile(Film* &films, int n) {
-    FILE* file = fopen("File", "w");
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file.\n");
-        return;
+    if (!found) {
+        cout << "Фильм с названием \"" << name << "\" не найден.\n";
     }
-    fwrite(films, sizeof(struct Film), n, file);
-    fclose(file);
 }
+void searchFilmByYear(Film *a[], int n, int year) {
+    bool found = false;
+    for (int i = 0; i < n; i++) {
+        if (a[i]->year  == year) {
+            cout << "Фильмы \"" << year << "\" года найдены:\n";
+            cout << i + 1 <<a[i]->name << "\n";
+            found = true;
+        }
+    }
+    if (!found) {
+        cout << "Фильм с названием \"" << year << "\" не найден.\n";
+    }
+}
+void deleteFilm(Film* &films, int &n, int index) {
+    if(n>0){
+        if (index < 0 || index >= n) {
+            cout << "Invalid index\n";
+            return;
+        }
+        int k = 1;
+        freeFilm(films,k);
+        for (int i = index; i < n - 1; i++) {
+            films[i] = films[i + 1];
+        }
+        n--;
+    }
+}
+void redactFilm(Film* films, int n, int index) {
+    if(n>0){
+        if (index < 0 || index >= n) {
+            cout << "Invalid index\n";
+            return;
+        }
+        cout << "Введите новое название фильма: ";
+        string name;
+        getline(cin, name);
+        delete[] films[index].name;
+        films[index].name = new char[name.length() + 1];
+        strcpy(films[index].name, name.c_str());
+        cout << "Введите новый год выпуска: ";
+        cin >> films[index].year;
+        cin.ignore(); // Очистка буфера после ввода числа
+    }
+   
+}
+//void writeFilmsToFile(const char* filename, Film *a[], int n) {
+//    ofstream outFile(filename); // Открытие файла для записи
+//    if (!outFile.is_open()) {
+//        cout << "Ошибка открытия файла для записи." << endl;
+//        return;
+//    }
+//
+//    // Запись данных в файл
+//    for (int i = 0; i < n; ++i) {
+//        outFile << a[i]->name << "," << a[i]->year << endl;
+//    }
+//
+//    outFile.close(); // Закрытие файла
+//}
+
+//void readFilmsFromFile(const char* filename, Film** a, int& n) {
+//    ifstream inFile(filename); // Открытие файла для чтения
+//    if (!inFile.is_open()) {
+//        cout << "Ошибка открытия файла для чтения." << endl;
+//        return;
+//    }
+//
+//    // Считывание данных из файла
+//    n = 0; // Сброс счетчика фильмов
+//    char buffer[256]; // Буфер для считывания строки из файла
+//    while (inFile.getline(buffer, 256)) {
+//        char* token = strtok(buffer, ","); // Разделение строки по запятой
+//        a[n] = new Film;
+//        a[n]->name = new char[strlen(token) + 1];
+//        strcpy(a[n]->name, token);
+//        token = strtok(NULL, ",");
+//        a[n]->year = atoi(token);
+//        n++; // Увеличение счетчика фильмов
+//    }
+//
+//    inFile.close(); // Закрытие файла
+//}
 
